af91b9ba60147d7a005aa43bbdb015f9f37bf63b
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2023-01-18 12:00:22 -0500

    Update Get-EOLCalendarPermissions.ps1

--- Get-EOLCalendarPermissions.ps1
+++ Get-EOLCalendarPermissions.ps1
@@ -19,7 +19,7 @@ Catch {Exit}
 # Variables used for output file.
 $Timestamp = Get-Date -Format MMddyyyyHHmmss
 $OutputFileName = ($MyInvocation.MyCommand.Name).Replace(".ps1","") + "-$Timestamp"
-$OutputFilePath = "$PSScriptRoot\$OutputFileName.txt"
+$OutputFilePath = "$PSScriptRoot\$Timestamp-$OutputFileName.txt"
 
 $CalendarPermissions = @()
 
